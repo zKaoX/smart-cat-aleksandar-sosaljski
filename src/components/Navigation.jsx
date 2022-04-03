68dc05782ff28803a4a06ca35066297f76fc5bad
Aleksandar Sosaljski <zasadasda11@gmail.com>
2022-04-03 19:59:50 +0200

    Dodat css za navigaciju

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -8,10 +8,10 @@ function Navigation() {
 
     return (
         <nav>
-            <ul>
-                <li><NavLink style={({ isActive }) => isActive ? activeStyle : undefined} to="/products">Products</NavLink></li>
-                <li><NavLink style={({ isActive }) => isActive ? activeStyle : undefined} to="/about">About</NavLink></li>
-                <li><NavLink style={({ isActive }) => isActive ? activeStyle : undefined} to="/statistics">Statistics</NavLink></li>
+            <ul className='navigation'>
+                <li><NavLink to="/products">Products</NavLink></li>
+                <li><NavLink to="/about">About</NavLink></li>
+                <li><NavLink to="/statistics">Statistics</NavLink></li>
             </ul>
         </nav>
     );
